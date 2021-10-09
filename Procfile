0880f23e125762278617b33beedd6a08599942d4
Filipe <filipefernandes000@gmail.com>
2021-10-09 18:28:36 -0300

    ci: heroku deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run dist/main.js
+web: npm run start:prod
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     const port = process.env.PORT || 3333;
     await app.listen(port, () => console.log(`app listening on port ${port}`));
 }
