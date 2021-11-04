3f5d54b19323b6fad3cd11e001f677894c87a658
Filipe <filipefernandes000@gmail.com>
2021-11-03 22:09:13 -0300

    feat: add errorMessages constant

---
+++ src/shared/error-messages.ts
@@ -0,0 +1,8 @@
+export const errorMessages = {
+    //User
+    USER: {
+        USER_NOT_FOUND: 'User not found',
+        EMAIL_ALREADY_EXISTS: 'Email already exists',
+        CPF_ALREADY_EXISTS: 'CPF already exists',
+    },
+};
